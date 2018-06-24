67c8b93e5a33bf3f16df115522ee60ea806e6552
mediavista <mediavista7@gmail.com>
2018-06-24 18:59:10 +0700

    Update indonesian.php

--- indonesian.php
+++ indonesian.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * WHMCS Language File
+ * English (en)
+ *
+ * Please Note: These language files are overwritten during software updates
+ * and therefore editing of these files directly is not advised. Instead we
+ * recommend that you use overrides to customise the text displayed in a way
+ * which will be safely preserved through the upgrade process.
+ *
+ * For instructions on overrides, please visit:
+ *   http://docs.whmcs.com/Language_Overrides
+ *
+ * @package    WHMCS
+ * @author     WHMCS Limited <development@whmcs.com>
+ * @copyright  Copyright (c) WHMCS Limited 2005-2015
+ * @license    http://www.whmcs.com/license/ WHMCS Eula
+ * @version    $Id$
+ * @link       http://www.whmcs.com/
+ */
 
 if (!defined("WHMCS")) die("This file cannot be accessed directly");
 
